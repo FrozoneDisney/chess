49ee434bd1fca9cdc00f714a82c0b52738a63e6d
FrozoneDisney <kiiman.h@hotmail.com>
2021-02-22 20:58:56 +0100

    some changes

--- script.js
+++ script.js
@@ -27,6 +27,8 @@ function rendertext()
     {
         clearInterval(countdown);
         audio.play();
+        audio.volume = 2;
+        container.style.backgroundImage = "url('images/fb8.jpg')";
 
     }
 }
